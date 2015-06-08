774ed6f909fca5bba4fe46aa4f9e0062037bfe00
Vinh Le <vinh.le@zalora.vn>
2015-06-08 11:02:23 +0700

    fix prism bug

--- src/js/NgPrism.js
+++ src/js/NgPrism.js
@@ -16,7 +16,7 @@ angular.module('ngPrism', []).directive('nagPrism', ['$rootScope', function($roo
                 }
 
                 console.log(val, codeElement[0]);
-                element.attr('data-line', null);
+                element.attr('data-line', 0);
                 Prism.highlightElement(codeElement[0]);
             });
 
